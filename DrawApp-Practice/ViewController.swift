9841df59768dbe7feda9d9782be81d1c6c378894
oomuro <t-oomuro@winas.jp>
2019-01-08 19:12:40 +0900

    Initial Commit

---
+++ DrawApp-Practice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DrawApp-Practice
+//
+//  Created by 大室 on 2019/01/08.
+//  Copyright © 2019年 大室. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
